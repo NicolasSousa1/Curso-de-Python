891c4b20bad935139eea9ae5cfe5558f78cc2112
Nicolas de Sousa <160195971+NicolasSousa1@users.noreply.github.com>
2026-05-07 20:20:39 -0300

    Create ex019vetor.py

---
+++ Módulo 1/ex019vetor.py
@@ -0,0 +1,12 @@
+import random
+
+print("Hello world!")
+
+i = 0
+nomes = ['', '', '', '']
+
+while i < 4:
+    nomes[i] = str(input("Digite o Nome de um aluno: "))
+    i = i + 1
+
+print("O sorteado foi {}".format(random.choice(nomes)))
