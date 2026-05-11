b6a37d030dbd6f14cf33af799bf3ac7164508952
Nicolas de Sousa <160195971+NicolasSousa1@users.noreply.github.com>
2026-05-11 19:57:42 -0300

    Update ex032.py

--- Módulo 1/ex032.py
+++ Módulo 1/ex032.py
@@ -1,7 +1,10 @@
+from datetime import date
+
 print("Hello world!")
 
-ano = int(input("Digite um ano: "))
-
+ano = int(input("Digite um ano. se quiser analisar o ano atual digite 0 ou o ano atual: "))
+if ano == 0:
+    ano = date.today().year
 if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
     print("{} é um ano bissexto".format(ano))
 else:
